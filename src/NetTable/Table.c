bdab453251160467d5ece7cf5d8a07b416a01746
Matt Fichman <matt.fichman@gmail.com>
2015-12-24 11:38:59 -0500

    Update

--- src/NetTable/Table.c
+++ src/NetTable/Table.c
@@ -1,5 +1,13 @@
 #include "Primitives.h"
+#include "String.h"
 
 Bool NetTable_Table__equal(Object self, Object other) {
     return self == other;
+}
+
+String NetTable_Table_str__g(Object self) {
+    String ret = String_alloc(64);
+    sprintf(ret->data, "%p", self);
+    ret->length = strlen(ret->data);
+    return ret;
 }
